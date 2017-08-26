f5331c7a354e4f8c50ab0e9c26d885e16c03f380
15331094 <1617899539@qq.com>
2017-08-27 00:04:19 +0800

    月历要可以了

--- static/js/calender.js
+++ static/js/calender.js
@@ -1,7 +1,14 @@
 let ran = new Array();
 let urls = new Array()
 $(function() {
-    $.get("/get_month_task?username=gzm1997", function(data, status) {
+    //获取url中的参数
+    function getUrlParam(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+        var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+        if (r != null) return unescape(r[2]); return null; //返回参数值
+    }
+
+    $.get("/get_month_task?username=" + getUrlParam("username"), function(data, status) {
         for (x in data) {
             ran[parseInt(x) - 1] = data[x].num;
             urls[parseInt(x) - 1] = data[x].url
